12e042624579b1fc67b4ae69728a39989b39c7d0
William Hermozo <william.hermozo@protonmail.com>
2020-05-29 12:04:03 +0200

    set up scss files

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
+import "./styles/App.scss";
+
 ReactDOM.render(
   <BrowserRouter>
     <App />
