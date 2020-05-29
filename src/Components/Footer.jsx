1ceace21143cecd7cbfc4ab71a01fc74fcf12011
William Hermozo <william.hermozo@protonmail.com>
2020-05-29 12:03:35 +0200

    add section components

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p className="footer__credits">- Copyright williamh.io © 2020 -</p>
+      <p className="footer__credits">Designed and coded from scratch.</p>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/Main.jsx
+++ src/Main.jsx
@@ -4,6 +4,7 @@ import Header from './Components/Header';
 import Projects from './Components/Projects';
 import About from './Components/About';
 import Contact from './Components/Contact';
+import Footer from './Components/Footer';
 
 const Main = () => {
   return (
@@ -14,10 +15,7 @@ const Main = () => {
         <About />
         <Contact />
       </main>
-      <footer className="footer">
-        <p className="footer__credits">- Copyright williamh.io © 2020 -</p>
-        <p className="footer__credits">Designed and coded from scratch.</p>
-      </footer>
+      <Footer />
     </>
   );
 };
