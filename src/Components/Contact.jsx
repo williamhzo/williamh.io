d0ce9c8dea8d7a24b9e2bbaeae4d856cae1be9db
William Hermozo <william.hermozo@protonmail.com>
2020-05-29 18:22:57 +0200

    add styling

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 
 const Contact = () => {
+  const emailName = 'hi';
+  const emailProvider = 'williamh';
+  const emailDomain = 'io';
+
+  const copyToClipboard = () => {};
+
   return (
     <section className="contact">
       <h4 className="contact__title">
@@ -10,29 +16,36 @@ const Contact = () => {
         <ul className="contact__info">
           <li className="contact__item">
             I’m currently accepting freelance work. Feel free to reach out, I’ll
-            gladly get back to you ASAP.
+            gladly get back to you as soon as possible.
           </li>
           <li className="contact__item">
             Send me an email at{' '}
-            <input
-              className="contact__email"
-              readOnly="readonly"
-              placeholder="hi@williamh.io"
-            ></input>{' '}
+            <span className="contact__email">{`${emailName}@${emailProvider}.${emailDomain}`}</span>{' '}
             <span>(click to copy)</span>
           </li>
           <li className="contact__item">
             Stalk me on{' '}
-            <a href="/" className="contact__link">
+            <a
+              href="https://www.linkedin.com/in/williamhermozo/"
+              className="contact__link"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               LinkedIn
             </a>{' '}
             or check out my code on{' '}
-            <a href="/" className="contact__link">
+            <a
+              href="https://www.github.com/williamhzo/"
+              className="contact__link"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               GitHub
             </a>
             .
           </li>
         </ul>
+        <hr className="projects__separator"></hr>
       </div>
     </section>
   );
