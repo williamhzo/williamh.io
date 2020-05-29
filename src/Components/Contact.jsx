a5aff0f1dda1b90bc3db88d66935738a9e430d91
William Hermozo <william.hermozo@protonmail.com>
2020-05-29 18:46:45 +0200

    add copy email feature

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Contact = () => {
+  const [copy, setCopy] = useState(false);
+
   const emailName = 'hi';
   const emailProvider = 'williamh';
   const emailDomain = 'io';
 
-  const copyToClipboard = () => {};
+  const stringToCopy = `${emailName}@${emailProvider}.${emailDomain}`;
+
+  const copyToClipboard = () => {
+    if (navigator.clipboard) {
+      navigator.clipboard.writeText(stringToCopy); // short and latest version for the code below
+      setCopy(true);
+    } else {
+      const el = document.createElement('textarea'); // Create a <textarea> element
+      el.value = stringToCopy; // Set its value to the string that you want copied
+      el.setAttribute('readonly', ''); // Make it readonly to be tamper-proof
+      el.style.position = 'absolute';
+      el.style.left = '-9999px'; // Move outside the screen to make it invisible
+      document.body.appendChild(el); // Append the <textarea> element to the HTML document
+      const selected =
+        document.getSelection().rangeCount > 0 // Check if there is any content selected previously
+          ? document.getSelection().getRangeAt(0) // Store selection if found
+          : false; // Mark as false to know no selection existed before
+      el.select(); // Select the <textarea> content
+      document.execCommand('copy'); // Copy - only works as a result of a user action (e.g. click events)
+      document.body.removeChild(el); // Remove the <textarea> element
+      if (selected) {
+        // If a selection existed before copying
+        document.getSelection().removeAllRanges(); // Unselect everything on the HTML document
+        document.getSelection().addRange(selected); // Restore the original selection
+      }
+      setCopy(true);
+    }
+  };
 
   return (
     <section className="contact">
@@ -20,8 +49,11 @@ const Contact = () => {
           </li>
           <li className="contact__item">
             Send me an email at{' '}
-            <span className="contact__email">{`${emailName}@${emailProvider}.${emailDomain}`}</span>{' '}
-            <span>(click to copy)</span>
+            <span
+              onClick={copyToClipboard}
+              className="contact__email"
+            >{`${emailName}@${emailProvider}.${emailDomain}`}</span>{' '}
+            <span className="contact__email--copy">{copy ? '(copied!)' : '(click to copy)'}</span>
           </li>
           <li className="contact__item">
             Stalk me on{' '}
