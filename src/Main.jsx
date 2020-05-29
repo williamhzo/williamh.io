76d31fe06d99f69ba645b5a98dc6528e731477f0
William Hermozo <william.hermozo@protonmail.com>
2020-05-29 12:53:19 +0200

    add styles import

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 
+// components
 import Header from './Components/Header';
 import Projects from './Components/Projects';
 import About from './Components/About';
 import Contact from './Components/Contact';
 import Footer from './Components/Footer';
+
+// styles
+import './styles/App.scss';
+import './styles/Header.scss';
+import './styles/Header.scss';
+import './styles/Projects.scss';
+import './styles/About.scss';
+import './styles/Contact.scss';
+import './styles/Footer.scss';
 
 const Main = () => {
   return (
