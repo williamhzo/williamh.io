6355de426bbc516d1d0f13fdd3b7b73bc6e7d7ed
William Hermozo <william.hermozo@protonmail.com>
2020-05-29 12:53:32 +0200

    add wrapper div

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -8,14 +8,16 @@ const Header = () => {
       <h1 className="header__logo">
         William<br></br>Hermozo
       </h1>
-      <h2 className="header__title">
-        I’m a Front-End Developer & Web Designer.
-      </h2>
-      <h3 className="header__subtitle">
-        I design & code meaningful, user-centered applications for the modern
-        web.
-      </h3>
-      <SocialLinks />
+      <div className="header__wrapper">
+        <h2 className="header__title">
+          I’m a Front-End Developer & Web Designer.
+        </h2>
+        <h3 className="header__subtitle">
+          I design & code meaningful, user-centered applications for the modern
+          web.
+        </h3>
+        <SocialLinks />
+      </div>
     </header>
   );
 };
