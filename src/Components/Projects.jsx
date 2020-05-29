74880017b95087278a03a949cac65919bf45969f
William Hermozo <william.hermozo@protonmail.com>
2020-05-29 17:06:16 +0200

    fix classname issue

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -7,24 +7,60 @@ const Projects = () => {
         selected<br></br>projects.
       </h4>
       <div className="projects__wrapper">
-        <h5 className="projects__title">freely</h5>
-        <p className="projects__description">
-          React.js, Node.js, Express, MongoDB
-        </p>
+        <h5 className="projects__subtitle">
+          <a
+            href="https://www.freely.cool/"
+            rel="noopener noreferrer"
+            target="_blank"
+            className="projects__link"
+          >
+            freely
+          </a>
+        </h5>
+        <div className="projects__description">
+          <p className="projects__technologies">
+            React.js, Node.js, Express, MongoDB
+          </p>
+          <p className="projects__info">PWA, mobile-first | 2020</p>
+        </div>
         <hr className="projects__separator"></hr>
       </div>
       <div className="projects__wrapper">
-        <h5 className="projects__title">freely</h5>
-        <p className="projects__description">
-          React.js, Node.js, Express, MongoDB
-        </p>
+        <h5 className="projects__subtitle">
+          <a
+            href="https://scoby-frw.herokuapp.com/"
+            rel="noopener noreferrer"
+            target="_blank"
+            className="projects__link"
+          >
+            scoby
+          </a>
+        </h5>
+        <div className="projects__description">
+          <p className="projects__technologies">
+            React.js, Node.js, Express, MongoDB
+          </p>
+          <p className="projects__info">Map-based, mobile-first | 2020</p>
+        </div>
         <hr className="projects__separator"></hr>
       </div>
       <div className="projects__wrapper">
-        <h5 className="projects__title">freely</h5>
-        <p className="projects__description">
-          React.js, Node.js, Express, MongoDB
-        </p>
+        <h5 className="projects__subtitle">
+          <a
+            href="https://williamhzo.github.io/emoji-quiz/"
+            rel="noopener noreferrer"
+            target="_blank"
+            className="projects__link"
+          >
+            emoji-quiz
+          </a>
+        </h5>
+        <div className="projects__description">
+          <p className="projects__technologies">
+            Vanilla JavaScript, HTML5, CSS3
+          </p>
+          <p className="projects__info">SPA | 2020</p>
+        </div>
         <hr className="projects__separator"></hr>
       </div>
     </section>
