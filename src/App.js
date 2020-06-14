56d0a3627d1e298be3773d4ebb59fb1eab1a81e7
William Hermozo <william.hermozo@protonmail.com>
2020-06-14 10:54:21 +0200

    add cursor component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+import Cursor from './Components/Cursor';
+
 import Main from './Main';
 
 function App() {
   return (
     <section className="app">
+      <Cursor />
       <Switch>
         <Route exact path="/" component={Main} />
       </Switch>
