0a8e2f5fd9202813c4f38b6ea8c0532cf12cf27d
William Hermozo <william.hermozo@protonmail.com>
2020-06-16 00:52:16 +0200

    remove line break

--- src/Components/AllProjects.jsx
+++ src/Components/AllProjects.jsx
@@ -7,18 +7,18 @@ import projects from '../data/projects.json';
 const Projects = () => {
   return (
     <section className="projects">
-      <h4 className="projects__title">
-        selected<br></br>projects.
-      </h4>
-      {projects.map((project, index) => (
-        <Project
-          key={index}
-          name={project.name}
-          technologies={project.technologies}
-          description={project.description}
-          link={project.link}
-        />
-      ))}
+      <h4 className="projects__title">selected projects.</h4>
+      <div className="projects__wrapper">
+        {projects.map((project, index) => (
+          <Project
+            key={index}
+            name={project.name}
+            technologies={project.technologies}
+            description={project.description}
+            link={project.link}
+          />
+        ))}
+      </div>
     </section>
   );
 };
