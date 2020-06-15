5b1f4f6e3d4fb1dc2937d177708896da10058bce
William Hermozo <william.hermozo@protonmail.com>
2020-06-15 14:43:05 +0200

    add project data to component

--- src/Components/AllProjects.jsx
+++ src/Components/AllProjects.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Project from './Project'
+import Project from './Project';
 
 import projects from '../data/projects.json';
 
@@ -12,69 +12,13 @@ const Projects = () => {
       </h4>
       {projects.map((project, index) => (
         <Project
+          key={index}
           name={project.name}
           technologies={project.technologies}
           description={project.description}
           link={project.link}
         />
       ))}
-      <div className="projects__wrapper">
-        <h5 className="projects__subtitle">
-          <a
-            href="https://www.freely.cool/"
-            rel="noopener noreferrer"
-            target="_blank"
-            className="projects__link"
-          >
-            freely
-          </a>
-        </h5>
-        <div className="projects__description">
-          <p className="projects__technologies">
-            React.js, Node.js, Express, MongoDB
-          </p>
-          <p className="projects__info">PWA, mobile-first | 2020</p>
-        </div>
-        <hr className="projects__separator"></hr>
-      </div>
-      <div className="projects__wrapper">
-        <h5 className="projects__subtitle">
-          <a
-            href="https://scoby-frw.herokuapp.com/"
-            rel="noopener noreferrer"
-            target="_blank"
-            className="projects__link"
-          >
-            scoby
-          </a>
-        </h5>
-        <div className="projects__description">
-          <p className="projects__technologies">
-            React.js, Node.js, Express, MongoDB
-          </p>
-          <p className="projects__info">Map-based, mobile-first | 2020</p>
-        </div>
-        <hr className="projects__separator"></hr>
-      </div>
-      <div className="projects__wrapper">
-        <h5 className="projects__subtitle">
-          <a
-            href="https://williamhzo.github.io/emoji-quiz/"
-            rel="noopener noreferrer"
-            target="_blank"
-            className="projects__link"
-          >
-            emoji-quiz
-          </a>
-        </h5>
-        <div className="projects__description">
-          <p className="projects__technologies">
-            Vanilla JavaScript, HTML5, CSS3
-          </p>
-          <p className="projects__info">SPA | 2020</p>
-        </div>
-        <hr className="projects__separator"></hr>
-      </div>
     </section>
   );
 };
---
+++ src/Components/Project.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Project = ({ name, technologies, description, link }) => {
+  return (
+    <div className="projects__wrapper">
+      <h5 className="projects__subtitle">
+        <a
+          href={link}
+          rel="noopener noreferrer"
+          target="_blank"
+          className="projects__link"
+        >
+          {name}
+        </a>
+      </h5>
+      <div className="projects__description">
+        <p className="projects__technologies">{technologies}</p>
+        <p className="projects__info">{description}</p>
+      </div>
+      <hr className="projects__separator"></hr>
+    </div>
+  );
+};
+
+export default Project;
