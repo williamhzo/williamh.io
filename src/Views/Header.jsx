7959a5339e15d2089809cb92ffcc2e793896de8d
William Hermozo <william.hermozo@protonmail.com>
2020-06-16 23:07:39 +0200

    add typewriter effect

---
+++ src/Views/Header.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import Typewriter from 'typewriter-effect';
+
+import SocialLinks from '../Components/SocialLinks';
+import MainTitle from '../Components/MainTitle';
+
+import socials from '../data/socials.json';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1 className="header__logo">
+        <Typewriter
+          onClick={(typewriter) => {
+            typewriter
+              .deleteAll()
+              .typeString('williamh.io')
+              .pauseFor(2500)
+              .typeString('WILLIAM HERMOZO')
+              .start();
+          }}
+          onInit={(typewriter) => {
+            typewriter
+              .typeString('williamh.io')
+              .pauseFor(2500)
+              .deleteAll()
+              .typeString('WILLIAM HERMOZO')
+              .start();
+          }}
+        />
+      </h1>
+      <div className="header__wrapper">
+        <MainTitle />
+        <ul className="header__list">
+          {socials.map((item, index) => (
+            <SocialLinks
+              key={index}
+              link={item.link}
+              shortName={item.shortName}
+              fullName={item.fullName}
+            />
+          ))}
+        </ul>
+      </div>
+      <h3 className="header__subtitle">
+        I design & code meaningful, user-centered applications for the modern
+        web.
+      </h3>
+    </header>
+  );
+};
+
+export default Header;
