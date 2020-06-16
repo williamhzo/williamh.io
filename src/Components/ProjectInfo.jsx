c2e102f28a9e06ad3a75af4bce50ed2c8527dc64
William Hermozo <william.hermozo@protonmail.com>
2020-06-16 23:57:35 +0200

    refactor css

--- src/Components/ProjectInfo.jsx
+++ src/Components/ProjectInfo.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Project = ({ name, technologies, description, link }) => {
   return (
-    <div>
+    <div className='projects__hover'>
       <h5 className="section__subtitle">
         <a
           href={link}
@@ -14,7 +14,7 @@ const Project = ({ name, technologies, description, link }) => {
         </a>
       </h5>
       <div className="projects__description">
-        <p className="projects__technologies">{technologies}</p>
+        {/* <p className="projects__technologies">{technologies}</p> */}
         <p className="projects__info">{description}</p>
       </div>
       <hr className="content__separator"></hr>
