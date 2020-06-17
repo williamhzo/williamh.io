59dd05b60b19aba1659e7cf62359fee4030637f0
William Hermozo <william.hermozo@protonmail.com>
2020-06-17 11:20:29 +0200

    minor tweaks on layout

--- src/Components/ProjectInfo.jsx
+++ src/Components/ProjectInfo.jsx
@@ -2,23 +2,23 @@ import React from 'react';
 
 const Project = ({ name, technologies, description, link }) => {
   return (
-    <div className='projects__hover'>
-      <h5 className="section__subtitle">
-        <a
-          href={link}
-          rel="noopener noreferrer"
-          target="_blank"
-          className="projects__link animate-cursor"
-        >
-          {name}
-        </a>
-      </h5>
+    <>
       <div className="projects__description">
+        <h5 className="section__subtitle">
+          <a
+            href={link}
+            rel="noopener noreferrer"
+            target="_blank"
+            className="projects__link animate-cursor"
+          >
+            {name}
+          </a>
+        </h5>
         {/* <p className="projects__technologies">{technologies}</p> */}
         <p className="projects__info">{description}</p>
       </div>
       <hr className="content__separator"></hr>
-    </div>
+    </>
   );
 };
 
